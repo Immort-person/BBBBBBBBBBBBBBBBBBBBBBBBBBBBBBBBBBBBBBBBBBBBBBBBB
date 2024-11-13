313200aae4ecbfd555a6b5fb05f17e19c884b1da
FESQO <98529236+FESQO@users.noreply.github.com>
2024-11-13 16:15:37 +1000

    Добавьте файлы проекта.

---
+++ BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB/Controllers/UsersController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace UserAuthApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserAuthContext _context;
+
+        public UsersController(UserAuthContext context)
+        {
+            _context = context;
+        }
+
+
+        //Все юзеры
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+
+        //Ищет юзера
+        [HttpGet("find")]
+        public async Task <ActionResult<User>> GetUser(string login)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Login == login);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+
+        //Создать юзера
+        [HttpPost]
+        public ActionResult<User> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+        }
+
+
+        //Редактировать
+        [HttpPut("{id}")]
+        public IActionResult PutUser(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Users.Any(e => e.Id == id))
+                {
+                    return NotFound();
+                }
+                throw;
+            }
+
+            return NoContent();
+        }
+
+
+
+        //БАН НАУЙ
+        [HttpDelete("{id}")]
+        public ActionResult<User> DeleteUser(int id)
+        {
+            var user = _context.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+
+            return user;
+        }
+    }
+}
---
+++ BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB/UserAuthContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB.Models
+{
+    public class UserAuthContext : DbContext
+    {
+        public UserAuthContext(DbContextOptions<UserAuthContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<User> Users { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=192.168.200.35;userid=user03;password=97965;database=user03", ServerVersion.Parse("10.3.39-mariadb"));
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public int IsBlocked { get; set; }
+        public int FailedLoginAttempts { get; set; }
+    }
+}
